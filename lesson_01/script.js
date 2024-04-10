e6fb23e9737b25a3a699d93c7fb45d3e890b63df
Aliaksei Barsukov <barsecky@gmail.com>
2024-04-10 19:05:56 +0300

    initialization modules

---
+++ lesson_01/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const productName = 'laptop';
+const productCount = 20;
+const productCategory = 'technique';
+const productPrice = 420;
+
+console.log('productName: ', productName);
+console.log('Общая стоимость товара: ', productCount * productPrice);
