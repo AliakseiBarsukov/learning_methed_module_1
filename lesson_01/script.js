02b38cf97cbfa0de151dfea8950bf9da0cf84008
Aliaksei Barsukov <barsecky@gmail.com>
2024-04-10 19:09:46 +0300

    add new product

--- lesson_01/script.js
+++ lesson_01/script.js
@@ -1,9 +1,21 @@
 'use strict';
 
-const productName = 'laptop';
-const productCount = 20;
-const productCategory = 'technique';
-const productPrice = 420;
+{
+  const productName = 'laptop';
+  const productCount = 20;
+  const productCategory = 'technique';
+  const productPrice = 420;
 
-console.log('productName: ', productName);
-console.log('Общая стоимость товара: ', productCount * productPrice);
+  console.log('productName: ', productName);
+  console.log('Общая стоимость товара: ', productCount * productPrice);
+}
+
+{
+  const productName = 'iPhone 15 pro max 512mb';
+  const productCount = 12;
+  const productCategory = 'phone';
+  const productPrice = 1350;
+
+  console.log('productName: ', productName);
+  console.log('Общая стоимость товара: ', productCount * productPrice);
+}
